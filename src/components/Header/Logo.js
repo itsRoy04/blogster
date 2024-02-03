5bb8dc20d07b33b44f9048f7122b113fc6ec2579
itsRoy04 <91062332+itsRoy04@users.noreply.github.com>
2024-02-03 13:38:25 +0530

    post blog

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 // import profileImg from "@/public/profile-img.png"
-import Capture from "@/public/Capture.png"
+
 
 const Logo = () => {
   return (
